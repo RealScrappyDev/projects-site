147480c0397fa6ddb0ecb04d4dd84048f7e6767a
Scrappy <scrappydev1979@gmail.com>
2024-09-08 08:02:25 -0500

    Add share image

--- constants.ts
+++ constants.ts
@@ -6,5 +6,5 @@ export const DEFAULT_TITLE = "Scrappy Dev's Projects";
 export const DEFAULT_TITLE_SUFFIX = " | Scrappy Dev";
 export const DEFAULT_DESCRIPTION = "A page for Scrappy's projects";
 export const DEFAULT_IMAGE_RELATIVE_PATH = "/scrappy.png";
-export const DEFAULT_SHARE_IMAGE_RELATIVE_PATH = DEFAULT_IMAGE_RELATIVE_PATH;
+export const DEFAULT_SHARE_IMAGE_RELATIVE_PATH = "/scrappy-projects-share.png";
 export const DEFAULT_X_SHARE_IMAGE_RELATIVE_PATH = DEFAULT_IMAGE_RELATIVE_PATH;
